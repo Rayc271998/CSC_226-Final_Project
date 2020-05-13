22e41eda8197a7378740f74af847ff0eb8add224
Briana Pacheco <brebell629@outlook.com>
2020-05-12 20:12:40 -0400

    Update login.inc.php
    Changed existing code, needs the table for testing.

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -5,34 +5,57 @@
     </head>
     <body>
         <?php
-            include "dbconnect.inc.php";
-            /* following code done by Briana, something wrong, I'm also confused --Wuqian_Ye */
-            if (isset($_POST['login'])) {
-                $user = trim(stripslashes(htmlspecialchars($_POST['username'])));
-                $pass = trim(stripslashes(htmlspecialchars($_POST['password'])));
-                $userCheck = false;
-                $passCheck = false;
-            
+        /* This PHP file processes information and can redirect to home.php after conditions are met, 
+		   so I think the HTML tags aren't needed.
+		
 
-                $query = "SELECT customerID FROM Customer WHERE email = $user AND password = $pass";
-                $response = mysqli_query($dbc, $query); //where $dbc is the database connetion
-                $row = mysql_fetch_array($response);
-                $active = $row['active'];
-                    
-                $count = mysqli_num_rows($result);
-                // If result matched $myusername and $mypassword, table row must be 1 row
-                        
-                if($count == 1) {
-                    session_register("user");
-                    $_SESSION['login_user'] = $myusername;
-                    header("location: home.php"); //Where they should be redirected
-                } else {
-                    $error = "Your Login Name or Password is invalid";
-                }
-            /* above code done by briana */
-            } else {
-                header("Location: ../login.php");
-            }
+		Session start done in header file. Two session values: 'loggedIn' and 'user'. They'll be our checks and 
+		how we address the user */
+
+		if(isset($_SESSION['loggedIn'] == true) //Checks if the user is logged in, then redirects to the home page.
+		{
+			header("location: ../home.php");
+			exit;
+		}
+
+		//Database connection file.
+		include "dbconnect.inc.php";
+		
+		if(isset($_POST['login'])) //If user clicked 'login'
+		{
+			//Collect information from POST and make a password check.
+			$username = trim(stripslashes(htmlspecialchars($_POST['username'])));
+			$password = trim(stripslashes(htmlspecialchars($_POST['password'])));
+			
+        
+			/* Start a query for username. We need to see if they exist in the database. 
+				Selects the entire row. */
+			$query1 = "SELECT * FROM Customers WHERE username = ?";
+			
+			$stmt = $conn->prepare($query1);
+			$stmt->bind_param("s", $username);
+			$stmt->execute();
+
+			/* Not entirely sure if this bottom bit is redundant, but the idea is to 
+			   get the result from $stmt and store it into $result, then store that information as an 
+			   associative array in another variable being $row, that way it can be worked 
+			   on with the retreived information without tampering with $result */
+
+			$result = $stmt->get_result();
+			$row = $result->fetch_assoc();
+
+			if($row['password'] == $password) //Successful Login Case, redirect to home.php.
+			{
+				$_SESSION['loggedIn'] = true;
+				$_SESSION['user'] = $row['username'];
+				header(loacation: "../home.php");
+			}
+			else //Incorrect Password Case, redirect back to login.
+			{
+				// $_SESSION['loggedIn'] = false; Not sure if needed.
+				header(location "../login.php");
+			}
+		}
         ?>
     </body>
 </html>
